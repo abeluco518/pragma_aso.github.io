99b25ddc6226084ad28aae6dc62b83a8ca4acb8a
abel <ababcantudo@gmail.com>
2025-10-02 20:02:25 +0200

    error in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         main: 'index.html',
         verification:'google76365b1ad045cc01.html',
         qui:'qui-som.html',
-        help:'help.html'
+        help:'help.html',
+        donate:'donate.html',
         
         
         // Añade aquí todos los html que quieras incluir
